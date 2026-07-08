7509631ecaa01f57014d06ca997de8a071efa28b
adounnenouz-beep <adounnenouz@gmail.com>
2026-07-08 22:52:50 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+MONETAG_AD_URL = os.getenv("MONETAG_AD_URL")
+WEBAPP_URL = os.getenv("WEBAPP_URL")
