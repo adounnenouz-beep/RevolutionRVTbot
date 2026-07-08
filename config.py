8ba14751cef506b15e46b6e0118076a4e62dc5a7
adounnenouz-beep <adounnenouz@gmail.com>
2026-07-08 23:19:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 MONETAG_AD_URL = os.getenv("MONETAG_AD_URL")
-WEBAPP_URL = os.getenv("WEBAPP_URL")
+WEBAPP_URL = os.getenv("WEBAPP_URL")  # هذا سيكون رابط Netlify
